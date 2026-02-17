11d4385bbd470f91e0098ae7c604b5ec5660b014
Guilherme Barros <guilherme.barros@newcastle.edu.au>
2026-02-18 10:55:59 +1100

    Remove what is not currently used in math lib

--- include/math/bessel.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-namespace math {
-double bessel_j0(double x);
-double bessel_j1(double x);
-} // namespace math
--- source/math/bessel.cpp
+++
@@ -1,9 +0,0 @@
-#include "math/bessel.hpp"
-#include <boost/math/special_functions/bessel.hpp>
-
-namespace math {
-
-double bessel_j0(double x) { return boost::math::cyl_bessel_j(0, x); }
-double bessel_j1(double x) { return boost::math::cyl_bessel_j(1, x); }
-
-} // namespace math
